b6603f5a70c4494147391a007ba890bd40f24030
Daniel Lagrava Sandoval <Daniel.LagravaSandoval@niwa.co.nz>
2019-06-05 14:38:01 +1200

    Added the base classes for the plugin that finally work!

---
+++ DeltaShell.Plugins.SosService/SosServiceApplicationPlugin.cs
@@ -0,0 +1,30 @@
+﻿using DelftTools.Shell.Core;
+using Mono.Addins;
+
+namespace DeltaShell.Plugins.SosService
+{
+    [Extension(typeof(IPlugin))]
+    public class SosServiceApplicationPlugin : ApplicationPlugin
+    {
+        public override string Name
+        {
+            get { return "SosServiceApplication"; }
+        }
+        public override string DisplayName
+        {
+            get { return "Sos Service application"; }
+        }
+        public override string Description
+        {
+            get { return "Application plugin to query SOS NIWA service"; }
+        }
+        public override string Version
+        {
+            get { return "1.0"; }
+        }
+        public override string FileFormatVersion
+        {
+            get { return "1.0"; }
+        }
+    }
+}
---
+++ DeltaShell.Plugins.SosService/SosServiceGuiPlugin.cs
@@ -0,0 +1,30 @@
+﻿using DelftTools.Shell.Core;
+using DelftTools.Shell.Gui;
+using Mono.Addins;
+namespace DeltaShell.Plugins.SosService
+{
+    [Extension(typeof(IPlugin))]
+    public class SosServiceGuiPlugin : GuiPlugin
+    {
+        public override string Name
+        {
+            get { return "SosServiceApplicationUI"; }
+        }
+        public override string DisplayName
+        {
+            get { return "Sos Service application (UI)"; }
+        }
+        public override string Description
+        {
+            get { return "Gui plugin to query SOS NIWA service"; }
+        }
+        public override string Version
+        {
+            get { return "1.0"; }
+        }
+        public override string FileFormatVersion
+        {
+            get { return "1.0"; }
+        }
+    }
+}

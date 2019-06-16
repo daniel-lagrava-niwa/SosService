bc7f4adead20fe4fb2298b0594c76836396a11a0
Daniel Lagrava Sandoval <Daniel.LagravaSandoval@niwa.co.nz>
2019-06-17 10:29:07 +1200

    PreQuery now does an actual request and the result is logged. Still need to find a good way to represent it.

--- DeltaShell.Plugins.SosService/Models/SosServicePreQuery.cs
+++ DeltaShell.Plugins.SosService/Models/SosServicePreQuery.cs
@@ -1,19 +1,31 @@
 ﻿using System;
 using DelftTools.Shell.Core.Workflow;
+using log4net;
 
 
 namespace DeltaShell.Plugins.SosService.Models
 {
     class SosServicePreQuery : ModelBase
     {
+        private readonly SOSClientJSON.JSONClient jsonClient;
+        private static readonly ILog Log = LogManager.GetLogger(typeof(SosServicePreQuery));     // handle for writing log messages
+
+        public SosServicePreQuery()
+        {
+            jsonClient = new SOSClientJSON.JSONClient("http://wellsensorobsp.niwa.co.nz:8080/52n-sos-aquarius-webapp/service");
+        }
+
         protected override void OnExecute()
         {
-            throw new NotImplementedException();
+            SOSClientJSON.Utils.StationsData stationData = jsonClient.PerformCapabilitiesRequest();
+            Log.Debug(stationData.ToString());
+
+            Status = ActivityStatus.Done;
         }
 
         protected override void OnInitialize()
         {
-            throw new NotImplementedException();
+            Console.WriteLine("Initialization step");
         }
     }
 }
